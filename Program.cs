9e0ffd1a89bd0c91d3227531657643a4dfd92a39
sethu <sethu@DESKTOP-24CTG5U>
2021-09-10 10:28:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// C# code to compute distance
+using System;
+
+class GFG
+{
+    // Function to calculate distance
+    static double distance(int x1, int y1, int x2, int y2)
+    {
+        // Calculating distance
+        return Math.Sqrt(Math.Pow(x2 - x1, 2) +
+                      Math.Pow(y2 - y1, 2) * 1.0);
+    }
+
+    // Driver code
+    public static void Main()
+    {
+        Console.WriteLine(Math.Round(distance(3, 4, 4, 3)
+                                   * 100000.0) / 100000.0);
+    }
+}
